b5b0e037ea1156cc5ca4d76f1167f69f9c13ffcc
MShirazAhmad <35538879+MShirazAhmad@users.noreply.github.com>
2018-02-25 17:45:14 +0500

    Update sukill.sh

--- sukill.sh
+++ sukill.sh
@@ -9,4 +9,10 @@ read password
 
 chmod +x "$FILEPATH"sukill
 
-%Run this file by using command "sudo bash ./sukill.sh
+#1 Run this file by using command "sudo bash ./sukill.sh" 
+#2 Enter the path to executable files: /bin/  (Press enter or edit path)
+#3 Enter User Account Password:  (Enter Password that you use for sudo)
+#4 Now, your file is generated and placed in proper location, to kill any running app, open terminal, and type "sukill "app_name"
+#5 This command ("sukill") will never ask for sudo, will use saved password of step 3 to kill "app_name"
+#6 Tested on LinuxMint 18.3
+#6 M. Shiraz Ahmad (shiraz.phy@gmail.com)
